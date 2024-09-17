8efa7712cd9fac9787a89338e05d0972af8b55c6
Felipe <felipe.sarmiento.as@gmail.com>
2024-09-17 14:21:31 -0400

    credentials ommited

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
 console.log(process.env.MONGODB_URI);
-mongoose.connect(
-  "mongodb+srv://felipesarmientoas:4oF6PgPruKTzUPiY@clusterintac.k58iu.mongodb.net/intac"
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/INTAC");
 // || "mongodb://127.0.0.1:27017/INTAC"
 module.exports = mongoose.connection;
